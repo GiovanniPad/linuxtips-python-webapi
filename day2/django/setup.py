26e29c7cdb3f79832798d171f88240d7e8abde82
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-05-29 21:16:07 -0300

    Day 2(Django: Hello World): Lesson complete
    Learning Django's core concepts and how to start and configure
    a new project and app using setuptools.

---
+++ day2/django/setup.py
@@ -0,0 +1,16 @@
+# Biblioteca para realizar o build de aplicações Python.
+from setuptools import setup
+
+# Função responsável pela configuração do projeto a ser buildado/instalado.
+setup(
+    # Nome da aplicação.
+    name="django_blog",
+    # Versão
+    version="0.1.0",
+    # Pacotes (módulos) necessários e que vão ser instalados no app.
+    packages=["djblog", "blog"],
+    # Dependências necessárias para executar o app.
+    requires=[
+        "django"
+    ],
+)
